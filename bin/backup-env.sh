1daa5bd5b6a24a44831e3e0d38929ddb3ec55936
francesco <info.francescobianco@gmail.com>
2023-07-10 16:31:03 +0200

    First commit

--- bin/backup-env.sh
+++ bin/backup-env.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-BACKUP_DATE=$(date -d "${BACKUP_DATE}" '+%y-%m-%d')
+BACKUP_DATE=$(date -d "${BACKUP_DATE}" '+%y-%m-%d %H:%M:%S')
 
 FILE_DATE=$(date -d "${BACKUP_DATE}" '+%Y%m%d_%H%M')
--- bin/backup-files.sh
+++ bin/backup-files.sh
@@ -26,40 +26,39 @@ if [[ $COMPRESS -eq 0 ]]; then
   TAR_FILE="${FILE_DATE}_files.tar"
 fi
 
-if [[ -d ${LOCAL_PATH} ]]; then
-  # Control will enter here if /data exists.
-  echo "[`date '+%Y-%m-%d %H:%M:%S'`] Archiving ${LOCAL_PATH} folder…"
-  $TAR $TAR_OPTIONS ${TMP_FOLDER}/${TAR_FILE} ${LOCAL_PATH}
+if [[ ! -d ${LOCAL_PATH} ]]; then
+  echo "[`date '+%Y-%m-%d %H:%M:%S'`] ${LOCAL_PATH} folder does not exists."
+  exit 1
+fi
 
-  # IF CHUNK_SIZE is greater than 0 we split tar archive in chunks for better stability using FTP transfers
-  # This allow transfer to resume if connection is lost using mirror command.
-  if [[ $CHUNK_SIZE -gt 0 ]];
-  then
-      echo "[`date '+%Y-%m-%d %H:%M:%S'`] Splitting ${TMP_FOLDER}/${TAR_FILE} into ${CHUNK_SIZE}MB parts.";
-      $MKDIR ${TMP_FOLDER}/${FILE_DATE}_files;
-      $SPLIT $SPLIT_OPTIONS ${TMP_FOLDER}/${TAR_FILE} ${TMP_FOLDER}/${FILE_DATE}_files/${TAR_FILE}.part;
-      # Sending over FTP
-      echo "[`date '+%Y-%m-%d %H:%M:%S'`] Sending ${TMP_FOLDER}/${FILE_DATE}_files folder over FTP using ${PARALLEL_UPLOADS} parallel uploads…";
-      # Use mirror for parallel upload (2), recursive upload and auto-resume
-      ${LFTP} ${LFTP_CMD} <<EOF
+# Control will enter here if /data exists.
+echo "[`date '+%Y-%m-%d %H:%M:%S'`] Archiving ${LOCAL_PATH} folder…"
+$TAR $TAR_OPTIONS ${TMP_FOLDER}/${TAR_FILE} ${LOCAL_PATH}
+
+# IF CHUNK_SIZE is greater than 0 we split tar archive in chunks for better stability using FTP transfers
+# This allow transfer to resume if connection is lost using mirror command.
+if [[ $CHUNK_SIZE -gt 0 ]]; then
+  echo "[`date '+%Y-%m-%d %H:%M:%S'`] Splitting ${TMP_FOLDER}/${TAR_FILE} into ${CHUNK_SIZE}MB parts.";
+  $MKDIR ${TMP_FOLDER}/${FILE_DATE}_files;
+  $SPLIT $SPLIT_OPTIONS ${TMP_FOLDER}/${TAR_FILE} ${TMP_FOLDER}/${FILE_DATE}_files/${TAR_FILE}.part;
+  # Sending over FTP
+  echo "[`date '+%Y-%m-%d %H:%M:%S'`] Sending ${TMP_FOLDER}/${FILE_DATE}_files folder over FTP using ${PARALLEL_UPLOADS} parallel uploads…";
+  # Use mirror for parallel upload (2), recursive upload and auto-resume
+  ${LFTP} ${LFTP_CMD} <<EOF
 cache flush;
 cd ${REMOTE_PATH};
 mirror -R -P ${PARALLEL_UPLOADS} ${TMP_FOLDER}/${FILE_DATE}_files ${FILE_DATE}_files;
 bye;
 EOF
-  else
-      # Sending over FTP
-      echo "[`date '+%Y-%m-%d %H:%M:%S'`] Sending ${TMP_FOLDER}/${TAR_FILE} file over FTP…";
-      ${LFTP} ${LFTP_CMD} <<EOF
+else
+  # Sending over FTP
+  echo "[`date '+%Y-%m-%d %H:%M:%S'`] Sending ${TMP_FOLDER}/${TAR_FILE} file over FTP";
+  ${LFTP} ${LFTP_CMD} <<EOF
 cache flush;
 cd ${REMOTE_PATH};
 put ${TMP_FOLDER}/${TAR_FILE};
 bye;
 EOF
-  fi
-
-  echo "[`date '+%Y-%m-%d %H:%M:%S'`] Files upload was done."
-else
-  echo "[`date '+%Y-%m-%d %H:%M:%S'`] ${LOCAL_PATH} folder does not exists."
-  exit 1
 fi
+
+echo "[`date '+%Y-%m-%d %H:%M:%S'`] Files upload was done."
